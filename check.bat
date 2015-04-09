52346082f4157502173be79f02e279c75917468c
miemiekurisu <miemiekurisu@aol.com>
2015-04-09 11:26:21 +0800

    modify bat file

--- check.bat
+++ check.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 rem Add tesseract ocr program to PATH
-PATH D:\Python27\;D:\Python27\Scripts;C:\Program Files\Broadcom\Broadcom 802.11 Network Adapter\Driver;C:\Program Files\Intel\iCLS Client\;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\;C:\Program Files\Intel\Intel(R) Management Engine Components\DAL;C:\Program Files\Intel\Intel(R) Management Engine Components\IPT;C:\Program Files\Intel\OpenCL SDK\2.0\bin\x86;D:\Program Files\TortoiseSVN\bin;c:\Program Files\Lenovo\Lenovo Home\;%M2_HOME%\bin;.\Tesseract-OCR;d:\Python27\
+set PATH = %PATH%;.\Tesseract-OCR
 rem run python script
 python process.py
